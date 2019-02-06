938e7a4effe5ec3ca86a0b9e31ea4c32b78e4b4f
Diego Espinoza <diespinozas@gmail.com>
2019-02-06 14:42:34 -0500

    add basic styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Bar, Pie } from 'react-chartjs-2';
+import styled from 'styled-components';
+
+const MainView = styled.div`
+  display: flex;
+  /* justify-content: space-around; */
+  > div {
+    flex-grow: 1;
+  }
+`;
 
 class App extends Component {
   constructor() {
@@ -82,25 +91,31 @@ class App extends Component {
     return (
       <div>
         <h1>Budget Dashboard</h1>
-        <div>
-          <select size="10" onChange={this.handleChange}>
-            <option value="601">
-              TOTAL SUBG. OPERACIÓN Y MANTENIMIENTO DISTRIB.
-            </option>
-            <option value="6012">DPTO MANTENIMIENTO INSTALACIONES MT</option>
-            <option value="60128">60128 EJECUCIÓN MANIOBRAS</option>
-            <option value="6013">DPTO MANTENIMIENTO INSTALACIONES BT</option>
-            <option value="60132">60132 MANTENIMIENTO MEDICION DIRECTA</option>
-            <option value="60133">
-              60133 INSTAL. Y MANT. EQUIP. MEDIC. INDIRECT
-            </option>
-            <option value="60137">
-              60137 OPERACIONES Y MANTENIMIENTO DISTRIB. SUR
-            </option>
-            <option value="60138">60138 EMERGENCIAS CAÑETE</option>
-            <option value="6014">DPTO ATENCIÓN DE EMERGENCIAS BT</option>
-            <option value="60142">60142 DIRECCION Y CONTROL SISTEMA BT </option>
-          </select>
+        <MainView>
+          <div>
+            <select size="10" onChange={this.handleChange}>
+              <option value="601">
+                TOTAL SUBG. OPERACIÓN Y MANTENIMIENTO DISTRIB.
+              </option>
+              <option value="6012">DPTO MANTENIMIENTO INSTALACIONES MT</option>
+              <option value="60128">60128 EJECUCIÓN MANIOBRAS</option>
+              <option value="6013">DPTO MANTENIMIENTO INSTALACIONES BT</option>
+              <option value="60132">
+                60132 MANTENIMIENTO MEDICION DIRECTA
+              </option>
+              <option value="60133">
+                60133 INSTAL. Y MANT. EQUIP. MEDIC. INDIRECT
+              </option>
+              <option value="60137">
+                60137 OPERACIONES Y MANTENIMIENTO DISTRIB. SUR
+              </option>
+              <option value="60138">60138 EMERGENCIAS CAÑETE</option>
+              <option value="6014">DPTO ATENCIÓN DE EMERGENCIAS BT</option>
+              <option value="60142">
+                60142 DIRECCION Y CONTROL SISTEMA BT{' '}
+              </option>
+            </select>
+          </div>
           <div>
             <Bar
               data={{
@@ -146,25 +161,27 @@ class App extends Component {
               }}
             />
           </div>
-          <Pie
-            data={{
-              labels: this.state.factLabels,
-              datasets: [
-                {
-                  backgroundColor: [
-                    '#FF6384',
-                    '#36A2EB',
-                    '#FFCE56',
-                    '#FF6384',
-                    '#36A2EB',
-                    '#FFCE56'
-                  ],
-                  data: this.state.facturacion
-                }
-              ]
-            }}
-          />
-        </div>
+          <div>
+            <Pie
+              data={{
+                labels: this.state.factLabels,
+                datasets: [
+                  {
+                    backgroundColor: [
+                      '#FF6384',
+                      '#36A2EB',
+                      '#FFCE56',
+                      '#FF6384',
+                      '#36A2EB',
+                      '#FFCE56'
+                    ],
+                    data: this.state.facturacion
+                  }
+                ]
+              }}
+            />
+          </div>
+        </MainView>
       </div>
     );
   }
